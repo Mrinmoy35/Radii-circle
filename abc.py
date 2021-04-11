b292425b5e95d5348fb910245f1c48bb061712f0
Mrinmoy35 <82281251+Mrinmoy35@users.noreply.github.com>
2021-04-11 13:14:59 +0530

    abc.py
    This will print,                                                                         
    The area of the circle with radius  1.1  is: 3.8013271108436504

--- abc.py
+++ abc.py
@@ -1,5 +1,5 @@
 # Radii-circle
-by mrinmoy polley
+
 from math import pi
 r = float(input ("Input the radius of the circle : "))
 print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
